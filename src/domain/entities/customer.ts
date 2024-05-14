39117e4735d0c1926437d4f88be2a79cc24387b2
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-05-14 19:34:48 -0300

    refactor(entity): turn `cutomer id` required

--- src/domain/entities/customer.ts
+++ src/domain/entities/customer.ts
@@ -2,7 +2,7 @@ import type { Balance } from "./balance";
 import type { CustomerType } from "./customer-type";
 
 export interface Customer {
-  id?: string;
+  id: string;
   fullName: string;
   document: string;
   email: string;
